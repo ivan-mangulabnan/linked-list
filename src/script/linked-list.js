804c94f796e273a4d60497b3ceea35e66f94d122
ivan <ivanmangulabnan19@gmail.com>
2025-05-22 10:53:11 +0800

    Add directory and module for linkedlist

---
+++ src/script/linked-list.js
@@ -0,0 +1,28 @@
+export class LinkedList {
+  #head;
+
+  prepend(value) {
+    let node = new Node();
+    node.value = value;
+
+    if (!this.#head) {
+      this.#head = node;
+      return;
+    }
+
+    let temp = this.#head;
+    node.next = temp;
+    this.#head = node;
+  }
+
+  get head () {
+    return this.#head;
+  }
+}
+
+class Node {
+  constructor () {
+    this.value = null;
+    this.next = null;
+  }
+}
